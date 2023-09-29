1ce0e3f4803ae21aee55fa8e5600f63aff7dcec2
lavakarteek <124698056+lavakarteek@users.noreply.github.com>
2023-09-29 14:29:37 +0530

    tic-tac-toe game.cpp

--- tic-tac-toe game.cpp
+++ tic-tac-toe game.cpp
@@ -49,52 +49,63 @@ bool isGameOver(const std::vector<std::vector<char>>& board, char player) {
 }
 
 int main() {
-    std::vector<std::vector<char>> board(3, std::vector<char>(3, ' ')); 
-    char currentPlayer = 'X'; 
-    int row, col;
-    bool gameInProgress = true;
+    char playAgain;
+    do {
+        std::vector<std::vector<char>> board(3, std::vector<char>(3, ' ')); 
+        char currentPlayer = 'X'; 
+        int row, col;
+        bool gameInProgress = true;
 
-    std::cout << "Welcome to Tic-Tac-Toe!" << std::endl;
+        std::cout << "Welcome to Tic-Tac-Toe!" << std::endl;
 
-    while (gameInProgress) {
-        printBoard(board);
-
-        std::cout << "Player " << currentPlayer << ", enter your move (row and column): ";
-        std::cin >> row >> col;
-
-        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
-            std::cout << "Invalid move! Try again." << std::endl;
-            continue;
-        }
-
-        board[row][col] = currentPlayer;
-
-        if (isGameOver(board, currentPlayer)) {
+        while (gameInProgress) {
             printBoard(board);
-            std::cout << "Player " << currentPlayer << " wins!" << std::endl;
-            gameInProgress = false; 
-        } else {
-            bool draw = true;
-            for (int i = 0; i < 3; ++i) {
-                for (int j = 0; j < 3; ++j) {
-                    if (board[i][j] == ' ') {
-                        draw = false;
+
+            std::cout << "Player " << currentPlayer << ", enter your move (row and column): ";
+            std::cin >> row >> col;
+
+            if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
+                std::cout << "Invalid move! Try again." << std::endl;
+                continue;
+            }
+
+            
+            board[row][col] = currentPlayer;
+
+            
+            if (isGameOver(board, currentPlayer)) {
+                printBoard(board);
+                std::cout << "Player " << currentPlayer << " wins!" << std::endl;
+                gameInProgress = false; 
+            } else {
+                
+                bool draw = true;
+                for (int i = 0; i < 3; ++i) {
+                    for (int j = 0; j < 3; ++j) {
+                        if (board[i][j] == ' ') {
+                            draw = false;
+                            break;
+                        }
+                    }
+                    if (!draw) {
                         break;
                     }
                 }
-                if (!draw) {
-                    break;
+                if (draw) {
+                    printBoard(board);
+                    std::cout << "It's a draw!" << std::endl;
+                    gameInProgress = false; 
+                } else {
+                    
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
                 }
             }
-            if (draw) {
-                printBoard(board);
-                std::cout << "It's a draw!" << std::endl;
-                gameInProgress = false; 
-            } else {
-                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-            }
         }
-    }
+
+        std::cout << "Do you want to play again? (y/n): ";
+        std::cin >> playAgain;
+        std::cin.ignore();
+    } while (playAgain == 'y' || playAgain == 'Y');
 
     return 0;
 }
